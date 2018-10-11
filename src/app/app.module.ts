9a6a5cf4a6cf7b05ab2b6e62966dd84d6e39b859
Francesca Ropolo <franci.ropolo@gmail.com>
2018-10-11 13:30:03 +0200

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { LogInPageComponent } from './pages/log-in-page/log-in-page.component';
+
+
+const routes: Routes = [
+  { path: 'login', component: LogInPageComponent},
+  { path: '**', component: NotFoundPageComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundPageComponent,
+    LogInPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/log-in-page/log-in-page.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './log-in-page.component.html',
+  styleUrls: ['./log-in-page.component.scss']
+})
+export class LogInPageComponent implements OnInit {
+
+  feedbackEnabled = false;
+  error = null;
+  processing = false;
+
+  login: any = {
+    email: '',
+    password: ''
+  }
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  submitForm(form) {
+    this.error = '';
+    this.feedbackEnabled = true;
+    if (form.valid) {
+      this.processing = true;
+      this.authService.login(this.login)
+        .then(() => {
+          this.router.navigate(['/']);
+        })
+        .catch((err) => {
+          this.error = err.error.code; // :-)
+          this.processing = false;
+          this.feedbackEnabled = false;
+        });
+    }
+  }
+
+}
