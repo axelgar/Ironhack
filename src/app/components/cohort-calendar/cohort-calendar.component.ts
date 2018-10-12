6f708ab05c97731a2e909bfd0b18f6125b54bc41
Francesca Ropolo <franci.ropolo@gmail.com>
2018-10-12 15:53:37 +0200

    favicon, working on the calendar

--- src/app/components/cohort-calendar/cohort-calendar.component.ts
+++ src/app/components/cohort-calendar/cohort-calendar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { CohortService } from 'src/app/services/cohort.service';
 
 @Component({
   selector: 'app-cohort-calendar',
@@ -7,9 +9,25 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CohortCalendarComponent implements OnInit {
   @Input() cohortId: any;
-  constructor() { }
+  id: string;
+  cohort: any;
+  error = false;
+
+  constructor(private cohortService: CohortService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+      .subscribe((params) => {
+        this.id = params.id;
+        this.cohortService.getCohort(this.id)
+          .then((result) => {
+            this.cohort = result;
+          })
+          .catch((error) => {
+            console.log(error);
+            this.error = true;
+          })
+      })
   }
 
 }
