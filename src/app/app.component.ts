93d2d050bc1e42af8cde0e5f957f07af97056210
Francesca Ropolo <franci.ropolo@gmail.com>
2018-10-18 15:26:48 +0200

    scss almost done plus header fixed on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { Router } from '@angular/router';
+
 import { AuthService } from './services/auth.service';
 import { Location } from '@angular/common'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'client';
@@ -29,10 +30,26 @@ export class AppComponent {
       this.anon = !user;
       return this.currentUser=this.authService.getUser()
     });
+    window.addEventListener('scroll', this.scroll, true); //third parameter
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.scroll, true);
   }
   
   logout() {
     this.authService.logout()
       .then(() => this.router.navigate(['/login']));
   }
+
+  scroll = (): void => {
+    const body = document.getElementsByTagName('body')[0];
+    var currentScrollPos = window.pageYOffset;
+    if (currentScrollPos > 35) {
+      body.classList.add('scrolled');
+    } else {
+      body.classList.remove('scrolled');
+    }
+  };
+
 }
